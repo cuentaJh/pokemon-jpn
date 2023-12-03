b75f0f61637b9556c98c55f5d2a5d19152e4d662
cuentaJh <cuentagithub0@gmail.com>
2023-12-03 12:19:12 -0500

    cambio de estilo

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def owner_list(request):
+
+    data_context = {
+        'nombre': 'Katty Paredes',
+        'edad': 20,
+        'dni': '54268659',
+        'pais': 'Perú',
+        'vigente': False,
+    }
+
+    return render(request,'owner_list.html', context={'data': data_context})
